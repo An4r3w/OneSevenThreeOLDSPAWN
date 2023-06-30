5d2496cd39c9552deac06063e812668c92ee593f
An4r3w <75725607+An4r3w@users.noreply.github.com>
2023-06-30 23:18:27 +0200

    Add files via upload

---
+++ OneSevenThreeOLDSPAWN/EventHandlers.cs
@@ -0,0 +1,18 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+namespace OneSevenThreeESC
+{
+    public class EventHandlers
+    {
+        public void OnSpawned(SpawnedEventArgs ev)
+        {
+            if (ev.Player.Role == RoleTypeId.Scp173)
+            {
+                ev.Player.Position = Door.Get(DoorType.Scp173Gate).Position;
+            }
+        }
+    }
+}
---
+++ OneSevenThreeOLDSPAWN/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using System;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace OneSevenThreeESC
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Singleton;
+
+        private EventHandlers events;
+
+        public override string Author => "An4r3w";
+
+        public override string Name { get; } = "PenautEasy";
+
+        public override string Prefix { get; } = "ezpenaut";
+
+        public override Version Version => new Version(3, 5, 0);
+
+        public override Version RequiredExiledVersion => new Version(7, 0, 0);
+
+        public override PluginPriority Priority => PluginPriority.Default;
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            Player.Spawned += events.OnSpawned;
+
+            Singleton = this;
+            events = new EventHandlers();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            Player.Spawned -= events.OnSpawned;
+
+            events = null;
+        }
+    }
+}
